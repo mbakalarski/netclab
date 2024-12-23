8bf02e11828394c2d9b58c3ac2b8c046a791fb00
Michal Bakalarski <michal.bakalarski@gmail.com>
2024-12-23 13:04:26 +0100

    next commit

--- vlab_install.sh
+++ vlab_install.sh
@@ -62,6 +62,7 @@ docker exec $cluster_node bash -c "sha256sum --check cni-plugins-linux-amd64-${v
 docker exec $cluster_node bash -c "cd /opt/cni/bin && tar xvzf /cni-plugins-linux-amd64-${version}.tgz"
 docker exec $cluster_node bash -c "rm /cni-plugins-linux-amd64-${version}.tgz"
 
+log "custom CNI plugin"
 docker cp cni/accept-bridge ${cluster_node}:/opt/cni/bin/accept-bridge
 docker exec $cluster_node bash -c "chown root:root /opt/cni/bin/accept-bridge"
 docker exec $cluster_node bash -c "chmod +x /opt/cni/bin/accept-bridge"
