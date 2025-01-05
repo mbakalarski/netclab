c6ceaab95ee9a0cd5276def21c9cf7881df60d84
Michal Bakalarski <michal.bakalarski@gmail.com>
2025-01-05 15:32:23 +0100

    LoadBalancer w/o loadbalancer folder

--- vlab_install.sh
+++ vlab_install.sh
@@ -4,9 +4,6 @@
 set -e
 
 
-DIR=$(dirname "$(realpath $0)")
-
-
 declare withkubevirt=false
 if [[ "$#" -eq 1 ]] && [[ ${1} = "nokubevirt" ]]; then withkubevirt=false; fi
 if [[ "$#" -eq 1 ]] && [[ ${1} = "kubevirt" ]]; then withkubevirt=true; fi
@@ -54,9 +51,38 @@ wait_dir_has_file "/etc/cni/net.d/" "10-kindnet.conflist"
 
 
 log "LoadBalancer"
-cd "${DIR}/loadbalancer/"
-source ./metallb.sh
-cd ${DIR}
+unset version
+version=$(basename $(curl -s -w %{redirect_url} https://github.com/metallb/metallb/releases/latest))
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/${version}/config/manifests/metallb-native.yaml
+
+unset timeout; timeout=3m
+log "Deploying, give me ${timeout}"
+kubectl -n metallb-system wait --for=jsonpath='{.status.numberReady}'=1 --timeout=${timeout} daemonset.apps/speaker
+
+kind_subnet=$(docker network inspect kind --format json | jq -r .[].IPAM.Config[].Subnet | grep -v \: | awk -F'/' '{printf $1}')
+prefix=$(echo "${kind_subnet}" | awk -F'.' -e '{printf $1"."$2"."$3}')
+
+cat <<EOT | kubectl apply -f -
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: lbpool
+  namespace: metallb-system
+spec:
+  addresses:
+  - ${prefix}.100 - ${prefix}.250
+EOT
+
+cat <<EOT | kubectl apply -f -
+apiVersion: metallb.io/v1beta1
+kind: L2Advertisement
+metadata:
+  name: l2adv
+  namespace: metallb-system
+spec:
+  ipAddressPools:
+    - lbpool
+EOT
 
 
 log "CNI plugins"
