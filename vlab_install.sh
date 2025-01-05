647933c98efbb249b7d908bc6d05562867ac53ca
Michal Bakalarski <michal.bakalarski@gmail.com>
2025-01-05 17:54:54 +0100

    next commit

--- vlab_install.sh
+++ vlab_install.sh
@@ -95,13 +95,6 @@ docker exec $cluster_node bash -c "cd /opt/cni/bin && tar xvzf /cni-plugins-linu
 docker exec $cluster_node bash -c "rm /cni-plugins-linux-amd64-${version}.tgz"
 
 log "custom CNI plugin"
-# if [[ -f "cni/accept-bridge" ]]; then
-#     cniplugin="cni/accept-bridge"
-# else
-#     curl -LOs "https://raw.githubusercontent.com/mbakalarski/vLab/main/cni/accept-bridge"
-#     cniplugin="accept-bridge"
-# fi
-# docker cp $cniplugin ${cluster_node}:/opt/cni/bin/accept-bridge
 docker exec $cluster_node bash -c "curl -Ls -o /opt/cni/bin/accept-bridge https://raw.githubusercontent.com/mbakalarski/vLab/main/cni/accept-bridge"
 docker exec $cluster_node bash -c "chown root:root /opt/cni/bin/accept-bridge"
 docker exec $cluster_node bash -c "chmod +x /opt/cni/bin/accept-bridge"
