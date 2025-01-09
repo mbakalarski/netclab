f66f2dab380aae5187f14163826c45b75bec7871
Michal Bakalarski <michal.bakalarski@gmail.com>
2025-01-09 13:07:02 +0100

    next commit

--- examples/ixia-c/tests/b2b.sh
+++ examples/ixia-c/tests/b2b.sh
@@ -6,4 +6,4 @@ export OTG_API="https://$(kubectl get svc otg-controller -o jsonpath='{.status.l
 export OTG_LOCATION_P1="p1"
 export OTG_LOCATION_P2="p2"
 
-otgen create flow --smac 6e:34:fb:b3:9e:2c --dmac 46:0d:10:13:d0:28 -s 192.168.1.2 -d 192.168.1.3 -p 80 --rate 1000 | otgen run -k --metrics flow | otgen transform --metrics flow --counters frames | otgen display --mode table
+otgen create flow -s 1.1.1.1 -d 2.2.2.2 -p 80 --rate 1000 | otgen run -k --metrics flow | otgen transform --metrics flow --counters frames | otgen display --mode table
