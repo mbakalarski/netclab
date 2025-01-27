7199a2f570d82f826a506041cc4d89e72b9a8c06
mbakalarski <michal.bakalarski@gmail.com>
2025-01-27 19:18:30 +0100

    netclab on minikube

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
-### netclab
+# netclab
 NETwork Containerized LAB is a tool for running testbeds with mix of VM and container images, e.g. Cisco csr1000v and Juniper cRPD or Nokia SR Linux.<br>
 It is based on kind Kubernetes cluster.
 Network topologies are defined via k8s manifest files.<br>
 KubeVirt is used for VM support and Multus to have more network interfaces.<br>
 
-Install docker at first.<br>
-Then install kubectl and kind tool, e.g.
+## Prerequisites
+Linux host or VM with docker (rootless) installed.<br>
+
+## Tool installation
+Install kubectl and minikube or kind tool, e.g.
 ```
 curl -LO "https://dl.k8s.io/release/$(curl -sL https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 ```
+
+minikube:
+```
+curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
+```
+or kind:
 ```
 unset version
 version=$(basename $(curl -s -w %{redirect_url} https://github.com/kubernetes-sigs/kind/releases/latest))
@@ -18,29 +28,33 @@ curl -Lo ./kind https://kind.sigs.k8s.io/dl/${version}/kind-linux-amd64
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 ```
-<br><br>
-Run netclab installation script:
-```
-bash <(curl -Ls "https://raw.githubusercontent.com/mbakalarski/netclab/main/netclab_install.sh") kubevirt
-```
-or
-```
-bash <(curl -Ls "https://raw.githubusercontent.com/mbakalarski/netclab/main/netclab_install.sh") nokubevirt
-```
+
 Expose VM images via http access, e.g.:
 ```
 docker run --name www -dt --mount type=bind,source=$HOME/images,target=/usr/share/nginx/html -p 8080:80 nginx:latest
 ```
 
+<br><br>
+Run netclab installation script:
+```
+bash <(curl -Ls "https://raw.githubusercontent.com/mbakalarski/netclab/main/netclab-kind.sh")
+```
+or
+```
+bash <(curl -Ls "https://raw.githubusercontent.com/mbakalarski/netclab/main/netclab-minikube.sh")
+```
+with ```kubevirt``` arg for netclab with VM images.<br>
 
+<br><br>
 To create topology:
 ```
 cd <Lab folder>
 kubectl apply -f ./manifests/
 ```
 
-
-Then connect to console:
+<br><br>
+Then connect to console.<br>
+For VMs install virtctl:
 ```
 unset version
 version=$(kubectl get kubevirt.kubevirt.io/kubevirt -n kubevirt -o=jsonpath="{.status.observedKubeVirtVersion}")
@@ -48,10 +62,12 @@ curl -L -o virtctl https://github.com/kubevirt/kubevirt/releases/download/${vers
 chmod +x virtctl
 sudo mv ./virtctl /usr/local/bin/virtctl
 ```
+and connect:
 ```
 virtctl console <router name>
 ```
-and/or for containerized router:
+
+For containerized router:
 ```
 kubectl exec -ti <router name> -- bash
 ```
---
+++ netclab-minikube.sh
@@ -0,0 +1,146 @@
+#!/bin/bash
+
+
+set -e
+
+
+declare withkubevirt=false
+if [[ "$#" -eq 1 ]] && [[ ${1} = "nokubevirt" ]]; then withkubevirt=false; fi
+if [[ "$#" -eq 1 ]] && [[ ${1} = "kubevirt" ]]; then withkubevirt=true; fi
+
+declare cluster_name="netclab"
+declare cluster_node="netclab"
+
+
+log(){ echo; echo "$(date -Is -u) $*"; }
+
+
+wait_dir_has_file(){
+    local dirpath="$1"
+    local filename="$2"
+    local -i timeout=30
+    if [[ -n ${3} ]]; then timeout=${3}; fi
+    local -i c=0
+
+    log "Test ${filename} in ${dirpath}; timeout ${timeout}"
+
+    while [[ ${timeout} -ge 0 ]]
+    do
+        echo -n "."
+        c=$(docker exec $cluster_node bash -c "ls -lt ${dirpath}" | grep ${filename} | wc -l)
+        if [[ $c -eq 1 ]]; then break ;fi
+        sleep 1
+        timeout=$(($timeout-1))
+    done
+    echo
+    docker exec $cluster_node bash -c "ls -lt ${dirpath}*${filename}"
+}
+
+
+custom_cni_plugin(){
+  local plugin="$1"
+  log "custom CNI plugin ${plugin}"
+  docker exec $cluster_node bash -c "curl -Ls -o /opt/cni/bin/${plugin} https://raw.githubusercontent.com/mbakalarski/netclab/main/cni-plugin/${plugin}"
+  docker exec $cluster_node bash -c "chown root:root /opt/cni/bin/${plugin}"
+  docker exec $cluster_node bash -c "chmod +x /opt/cni/bin/${plugin}"
+}
+
+
+log "k8s cluster ${cluster_name}"
+minikube delete -p ${cluster_name}
+# minikube start -p ${cluster_name} --feature-gates=CPUManager=true,CPUManagerPolicyOptions=true --extra-config=kubelet.cpuManagerPolicy=static
+minikube start -p ${cluster_name} --feature-gates=CPUManager=true,CPUManagerPolicyOptions=true
+
+
+wait_dir_has_file "/etc/cni/net.d/" "conflist"
+
+
+unset version
+version=$(basename $(curl -s -w %{redirect_url} https://github.com/metallb/metallb/releases/latest))
+log "MetalLB ${version}"
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/${version}/config/manifests/metallb-native.yaml
+
+unset timeout; timeout=3m
+log "Deploying, give me ${timeout}"
+kubectl -n metallb-system wait --for=jsonpath='{.status.numberReady}'=1 --timeout=${timeout} daemonset.apps/speaker
+
+unset k8s_netname; k8s_netname="netclab"
+k8s_subnet=$(docker network inspect ${k8s_netname} --format json | jq -r .[].IPAM.Config[].Subnet | grep -v \: | awk -F'/' '{printf $1}')
+unset prefix; prefix=$(echo "${k8s_subnet}" | awk -F'.' -e '{printf $1"."$2"."$3}')
+
+cat <<EOT | kubectl apply -f -
+apiVersion: metallb.io/v1beta1
+kind: IPAddressPool
+metadata:
+  name: lbpool
+  namespace: metallb-system
+spec:
+  addresses:
+  - ${prefix}.100 - ${prefix}.250
+EOT
+
+cat <<EOT | kubectl apply -f -
+apiVersion: metallb.io/v1beta1
+kind: L2Advertisement
+metadata:
+  name: l2adv
+  namespace: metallb-system
+spec:
+  ipAddressPools:
+    - lbpool
+EOT
+
+
+log "Install jq for custom CNI plugins"
+docker exec $cluster_node bash -c "apt-get -y update && apt-get -y install jq"
+custom_cni_plugin "accept-bridge"
+custom_cni_plugin "pod2pod"
+
+
+if ${withkubevirt}; then
+    log "Test nested virtualization on k8s node"
+    nested=$(docker exec $cluster_node bash -c 'cat /sys/module/kvm_intel/parameters/nested | tr -d "\n"')
+    echo "nested: ${nested}"
+fi
+
+running_in_vm=false; if grep -i hypervisor /proc/cpuinfo > /dev/null; then running_in_vm=true; fi
+
+if ${withkubevirt} && ( ! ${running_in_vm} || [[ ${nested} = "Y" ]] ); then
+    unset version
+    version=$(curl -s https://storage.googleapis.com/kubevirt-prow/release/kubevirt/kubevirt/stable.txt)
+    log "KubeVirt ${version}"
+    kubectl create -f "https://github.com/kubevirt/kubevirt/releases/download/${version}/kubevirt-operator.yaml"
+    kubectl create -f "https://github.com/kubevirt/kubevirt/releases/download/${version}/kubevirt-cr.yaml"
+
+    unset timeout
+    timeout=5m
+    log "Deploying, give me ${timeout}"
+    kubectl -n kubevirt wait --for=jsonpath='{.status.phase}'=Deployed --timeout=${timeout} kubevirts.kubevirt.io/kubevirt
+    kubectl -n kubevirt get kubevirts.kubevirt.io/kubevirt
+
+    unset version
+    version=$(basename $(curl -s -w %{redirect_url} https://github.com/kubevirt/containerized-data-importer/releases/latest))
+    log "KubeVirt CDI ${version}"
+    kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$version/cdi-operator.yaml
+    kubectl create -f https://github.com/kubevirt/containerized-data-importer/releases/download/$version/cdi-cr.yaml
+
+    unset timeout
+    timeout=3m
+    log "Deploying, give me ${timeout}"
+    kubectl -n cdi wait --for=jsonpath='{.status.phase}'=Deployed --timeout=${timeout} cdi/cdi
+    kubectl -n cdi get cdi/cdi
+fi
+
+
+unset version
+version=$(basename $(curl -s -w %{redirect_url} "https://github.com/k8snetworkplumbingwg/multus-cni/releases/latest"))
+log "Multus ${version}"
+kubectl apply -f https://raw.githubusercontent.com/k8snetworkplumbingwg/multus-cni/master/deployments/multus-daemonset-thick.yml
+wait_dir_has_file "/etc/cni/net.d/" "00-multus.conf" 120
+
+
+log "Multus default-network"
+kubectl apply -f "https://raw.githubusercontent.com/mbakalarski/netclab/main/config/multus-default.yaml"
+
+
+log "Done"
